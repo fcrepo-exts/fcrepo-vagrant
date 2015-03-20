9ed3e3fb689850a032c8598da26b41f7c46a9f86
Andrew Woods <awoods@duraspace.org>
2015-03-20 00:54:56 -0400

    Update Fuseki version and start-cmd

--- install_scripts/fuseki.sh
+++ install_scripts/fuseki.sh
@@ -1,23 +1,23 @@
 echo "Installing Fuseki"
 
-FUSEKI_VERSION=1.1.1
+FUSEKI_VERSION=1.1.2
 FUSEKI_HOME=/usr/share/fuseki
 
 mkdir $FUSEKI_HOME
 
 cd /tmp
 echo -n "Downloading Fuseki..."
-wget -q http://www.apache.org/dist/jena/binaries/jena-fuseki-"$FUSEKI_VERSION"-distribution.tar.gz
+wget -q http://www.apache.org/dist/jena/binaries/jena-fuseki1-"$FUSEKI_VERSION"-distribution.tar.gz
 echo " done"
-tar -xzvf jena-fuseki-"$FUSEKI_VERSION"-distribution.tar.gz
-cd jena-fuseki-"$FUSEKI_VERSION"
+tar -xzvf jena-fuseki1-"$FUSEKI_VERSION"-distribution.tar.gz
+cd jena-fuseki1-"$FUSEKI_VERSION"
 mv -v * $FUSEKI_HOME
 chown -hR tomcat7:tomcat7 $FUSEKI_HOME
 
 mkdir "$FUSEKI_HOME/test_data"
 ln -s $FUSEKI_HOME/fuseki /etc/init.d
 echo 'FUSEKI_HOME="$FUSEKI_HOME"' > /etc/default/fuseki
-echo "FUSEKI_ARGS=\"--update --loc=$FUSEKI_HOME/test_data /test\"" >> /etc/default/fuseki
+echo "FUSEKI_ARGS=\"--update --mem /test\"" >> /etc/default/fuseki
 
 /etc/init.d/fuseki start
 
