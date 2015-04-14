b0f0b8ae3266accb3ba1ca4331276b084852029f
Esmé Cowles <escowles@ucsd.edu>
2015-04-14 13:25:02 -0400

    Add an option to use enable auth with fcrepo-webapp-plus-rbacl webapp
    Resolves: https://jira.duraspace.org/browse/FCREPO-1462

--- install_scripts/fedora4.sh
+++ install_scripts/fedora4.sh
@@ -10,17 +10,24 @@ if [ -f "$SHARED_DIR/install_scripts/config" ]; then
   . $SHARED_DIR/install_scripts/config
 fi
 
+if [ "${FEDORA_AUTH}" = "true" ]; then
+  PLUS_DIR="-plus"
+  PLUS_FILE="-plus-rbacl"
+fi
+RELEASES="https://github.com/fcrepo4-labs/fcrepo-webapp${PLUS_DIR}/releases/download/fcrepo-webapp${PLUS_DIR}-${FEDORA_VERSION}"
+WEBAPP="fcrepo-webapp${PLUS_FILE}-${FEDORA_VERSION}.war"
+
 cd $HOME_DIR
 
 mkdir /var/lib/tomcat7/fcrepo4-data
 chown tomcat7:tomcat7 /var/lib/tomcat7/fcrepo4-data
 chmod g-w /var/lib/tomcat7/fcrepo4-data
 
-if [ ! -f "$DOWNLOAD_DIR/fcrepo-webapp-$FEDORA_VERSION.war" ]; then
-  echo -n "Downloading Fedora 4..."
-  wget -q -O "$DOWNLOAD_DIR/fcrepo-webapp-$FEDORA_VERSION.war" "https://github.com/fcrepo4/fcrepo4/releases/download/fcrepo-$FEDORA_VERSION/fcrepo-webapp-$FEDORA_VERSION.war"
+if [ ! -f "$DOWNLOAD_DIR/$WEBAPP" ]; then
+  echo -n "Downloading Fedora 4... ${FEDORA_VERSION}"
+  curl -L -s -o "$DOWNLOAD_DIR/$WEBAPP" "$RELEASES/$WEBAPP"
   echo " done"
 fi
-cp "$DOWNLOAD_DIR/fcrepo-webapp-$FEDORA_VERSION.war" /var/lib/tomcat7/webapps/fcrepo.war
+cp "$DOWNLOAD_DIR/$WEBAPP" /var/lib/tomcat7/webapps/fcrepo.war
 chown tomcat7:tomcat7 /var/lib/tomcat7/webapps/fcrepo.war
 service tomcat7 restart
--- install_scripts/tomcat7.sh
+++ install_scripts/tomcat7.sh
@@ -5,7 +5,13 @@
 # Tomcat
 apt-get -y install tomcat7 tomcat7-admin
 usermod -a -G tomcat7 vagrant
-sed -i '$i<user username="fedora4" password="fedora4" roles="manager-gui"/>' /etc/tomcat7/tomcat-users.xml
+sed -i '$i<role rolename="fedoraUser"/>
+$i<role rolename="fedoraAdmin"/>
+$i<role rolename="manager-gui"/>
+$i<user username="testuser" password="password1" roles="fedoraUser"/>
+$i<user username="adminuser" password="password2" roles="fedoraUser"/>
+$i<user username="fedoraAdmin" password="secret3" roles="fedoraAdmin"/>
+$i<user username="fedora4" password="fedora4" roles="manager-gui"/>' /etc/tomcat7/tomcat-users.xml
 
 # Make the ingest directory
 mkdir /mnt/ingest
