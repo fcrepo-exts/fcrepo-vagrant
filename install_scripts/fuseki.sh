4629a288ced8ed3d1d4588e35c29348730ed48ca
nruest <ruestn@gmail.com>
2015-04-15 11:14:12 -0400

    Move Fuseki install from stand alone server to deployable war. FCREPO-1467

--- install_scripts/fuseki.sh
+++ install_scripts/fuseki.sh
@@ -8,26 +8,19 @@ fi
 
 if [ ! -d $FUSEKI_HOME ]; then
   mkdir $FUSEKI_HOME
+  chown -hR tomcat7:tomcat7 $FUSEKI_HOME
 fi
 
-if [ ! -f "$DOWNLOAD_DIR/jena-fuseki1-$FUSEKI_VERSION-distribution.tar.gz" ]; then
+if [ ! -f "$DOWNLOAD_DIR/apache-jena-fuseki-"$FUSEKI_VERSION".tar.gz" ]; then
   echo -n "Downloading Fuseki..."
-  wget -q -O "$DOWNLOAD_DIR/jena-fuseki1-$FUSEKI_VERSION-distribution.tar.gz" "http://www.apache.org/dist/jena/binaries/jena-fuseki1-"$FUSEKI_VERSION"-distribution.tar.gz"
+  wget -q -O "$DOWNLOAD_DIR/apache-jena-fuseki-"$FUSEKI_VERSION".tar.gz" "http://www.apache.org/dist/jena/binaries/apache-jena-fuseki-"$FUSEKI_VERSION".tar.gz"
   echo " done"
 fi
 
 cd /tmp
-cp "$DOWNLOAD_DIR/jena-fuseki1-$FUSEKI_VERSION-distribution.tar.gz" /tmp
-tar -xzvf jena-fuseki1-"$FUSEKI_VERSION"-distribution.tar.gz
-cd jena-fuseki1-"$FUSEKI_VERSION"
-mv -v * $FUSEKI_HOME
-chown -hR tomcat7:tomcat7 $FUSEKI_HOME
-
-mkdir "$FUSEKI_HOME/test_data"
-ln -s $FUSEKI_HOME/fuseki /etc/init.d
-echo "FUSEKI_HOME=\"$FUSEKI_HOME\"" > /etc/default/fuseki
-echo "FUSEKI_ARGS=\"--update --loc=$FUSEKI_HOME/test_data /test\"" >> /etc/default/fuseki
-update-rc.d fuseki start 20 3 4 5 . stop 20 0 1 2 6 .
-
-/etc/init.d/fuseki start
-
+cp "$DOWNLOAD_DIR/apache-jena-fuseki-"$FUSEKI_VERSION".tar.gz" /tmp
+tar -xzvf apache-jena-fuseki-"$FUSEKI_VERSION".tar.gz
+cd apache-jena-fuseki-"$FUSEKI_VERSION"
+mv -v fuseki.war $FUSEKI_DEPLOY
+chown -hR tomcat7:tomcat7 $FUSEKI_DEPLOY/fuseki.war
+service tomcat7 restart
