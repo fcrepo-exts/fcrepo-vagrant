c9cb7d47e86dea980eb26270535d871b99cc5c8d
Andrew Woods <awoods@duraspace.org>
2015-12-28 13:09:27 -0500

    Add fcrepo-serialization project
    Resolves: https://jira.duraspace.org/browse/FCREPO-1863

--- install_scripts/fedora_camel_toolbox.sh
+++ install_scripts/fedora_camel_toolbox.sh
@@ -41,6 +41,13 @@ fi
 sed -i 's|fcrepo.authUsername=$|fcrepo.authUsername=fedoraAdmin|' /opt/karaf/etc/org.fcrepo.camel.fixity.cfg
 sed -i 's|fcrepo.authPassword=$|fcrepo.authPassword=secret3|' /opt/karaf/etc/org.fcrepo.camel.fixity.cfg
 
+# Serialization service
+if [ ! -f "/opt/karaf/etc/org.fcrepo.camel.serialization.cfg" ]; then
+   /opt/karaf/bin/client -u karaf -h localhost -a 8101 "feature:install fcrepo-serialization"
+fi
+sed -i 's|fcrepo.authUsername=$|fcrepo.authUsername=fedoraAdmin|' /opt/karaf/etc/org.fcrepo.camel.serialization.cfg
+sed -i 's|fcrepo.authPassword=$|fcrepo.authPassword=secret3|' /opt/karaf/etc/org.fcrepo.camel.serialization.cfg
+
 # Reindexing service
 if [ ! -f "/opt/karaf/etc/org.fcrepo.camel.reindexing.cfg" ]; then
    /opt/karaf/bin/client -u karaf -h localhost -a 8101 "feature:install fcrepo-reindexing"
