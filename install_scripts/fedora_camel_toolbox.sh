24f6aacdb8dcf6e942a925fecf009f57fd7c427d
nruest <ruestn@gmail.com>
2016-02-18 16:42:29 -0500

    Address FCREPO-1919

--- install_scripts/fedora_camel_toolbox.sh
+++ install_scripts/fedora_camel_toolbox.sh
@@ -35,9 +35,11 @@ if [ ! -f "/opt/karaf/etc/org.fcrepo.camel.audit.cfg" ]; then
 fi
 
 # Fixity service
+sleep 10
 if [ ! -f "/opt/karaf/etc/org.fcrepo.camel.fixity.cfg" ]; then
    /opt/karaf/bin/client -u karaf -h localhost -a 8101 "feature:install fcrepo-fixity"
 fi
+sleep 10
 sed -i 's|fcrepo.authUsername=$|fcrepo.authUsername=fedoraAdmin|' /opt/karaf/etc/org.fcrepo.camel.fixity.cfg
 sed -i 's|fcrepo.authPassword=$|fcrepo.authPassword=secret3|' /opt/karaf/etc/org.fcrepo.camel.fixity.cfg
 
