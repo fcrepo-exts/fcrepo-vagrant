374d38fd162579a5ace41aa85c3e85e42906623b
Jared Whiklo <jwhiklo@gmail.com>
2016-09-20 10:40:44 -0500

    Increment Fedora version for RC testing, disable solr-indexing
    Related to FCREPO-2232

--- install_scripts/fedora_camel_toolbox.sh
+++ install_scripts/fedora_camel_toolbox.sh
@@ -20,10 +20,10 @@ if [ ! -f "/opt/karaf/etc/org.fcrepo.camel.service.activemq.cfg" ]; then
 fi
 
 # Solr indexing
-if [ ! -f "/opt/karaf/etc/org.fcrepo.camel.indexing.solr.cfg" ]; then
-   /opt/karaf/bin/client -u karaf -h localhost -a 8101 "feature:install fcrepo-indexing-solr"
-fi
-sed -i 's|solr.baseUrl=http://localhost:8983/solr/collection1|solr.baseUrl=http://localhost:8080/solr/collection1|' /opt/karaf/etc/org.fcrepo.camel.indexing.solr.cfg
+#if [ ! -f "/opt/karaf/etc/org.fcrepo.camel.indexing.solr.cfg" ]; then
+#   /opt/karaf/bin/client -u karaf -h localhost -a 8101 "feature:install fcrepo-indexing-solr"
+#fi
+#sed -i 's|solr.baseUrl=http://localhost:8983/solr/collection1|solr.baseUrl=http://localhost:8080/solr/collection1|' /opt/karaf/etc/org.fcrepo.camel.indexing.solr.cfg
 
 # Triplestore indexing
 if [ ! -f "/opt/karaf/etc/org.fcrepo.camel.indexing.triplestore.cfg" ]; then
